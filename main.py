7332b5ebc4fce6309e049a19076771f96cdcbe53
Dawid Lubera <luberadawid@gmail.com>
2017-04-15 14:11:30 +0200

    Added gameover state

--- main.py
+++ main.py
@@ -41,6 +41,10 @@ def initButtons(textfield, itembutton):
 
     nextClientButton = TextFieldButton((50, screensize[1] - 150), (screensize[0] / 2 - 100, 100), 100, 'Next Client')
 
+    gameOverAlert = TextFieldButton((200, screensize[1] / 2 - 200), (screensize[0] - 400, 400), 200, 'Game Over')
+    gameOverButton = TextFieldButton((screensize[0] / 2 - 300, screensize[1] / 2 + 100), (200, 64), 50, 'Try again')
+    gameOverExit = TextFieldButton((screensize[0] / 2 + 100, screensize[1] / 2 + 100), (200, 64), 50, 'Exit')
+
     for i in range(3):
         for j in range(3):
             val = 1 + j + 3 * i
@@ -56,7 +60,7 @@ def initButtons(textfield, itembutton):
     buttonList.append(Button((start[0], start[1] + 2 * 80), 'Weigh', 40, 'longbutton.png', function=unveilWeigh))
     buttonArgs.append(itembutton)
 
-    return nextClientButton, RenderButton(buttonList), buttonArgs
+    return nextClientButton, (gameOverAlert, gameOverButton, gameOverExit), RenderButton(buttonList), buttonArgs
 
 def generateItem(itembutton):
     x = random.randint(0, screensize[0] / 2 - 150)
@@ -74,7 +78,6 @@ def generateItem(itembutton):
 
     itembutton.add((ItemButton((x, y), itemName, quantity, weigh)))
 
-
 pygame.init()
 screen = pygame.display.set_mode(screensize)
 pygame.display.set_caption('The Great Rainbow')
@@ -86,10 +89,14 @@ background.fill((250, 250, 250))
 otherUi = pygame.sprite.RenderPlain((DrawRect((0, 0, 0), (screensize[0] / 2, 0), (2, screensize[1]))))
 textfield = TextField((848, 120), (224, 64), 64, (190, 220, 165), (60, 85, 35))
 itembutton = RenderButton(())
-nextClientButton, buttons, buttonArgs = initButtons(textfield, itembutton)
+nextClientButton, gameOverScreen, buttons, buttonArgs = initButtons(textfield, itembutton)
 
 screen.blit(background, (0, 0))
 pygame.display.flip()
+
+numberOfItems = 0
+numberOfItemsDone = 0
+numberOfGoods = 0
 
 clock = pygame.time.Clock()
 
@@ -113,30 +120,53 @@ while True:
                     if(item.weighed and item.checkClick(pygame.mouse.get_pos())):
                         if(item.toWeigh):
                             screen.blit(background, item.rect)
+                            numberOfGoods += 1
+                            numberOfItemsDone += 1
                             itembutton.empty()
                         else:
-                            item.setQuantity(item.quantity - int(textfield.string))
+                            q = int(textfield.string)
+                            item.setQuantity(item.quantity - q)
                             clear(textfield)
                             if(item.quantity <= 0):
                                 screen.blit(background, item.rect)
+                                numberOfGoods += q
+                                numberOfItemsDone += 1
                                 itembutton.empty()
                                 if(item.quantity < 0):
                                     endticks = pygame.time.get_ticks()
-                                    state='GAMEOVER'
-                                    print('Game over')
+                                    state = 'GAMEOVER'
                                     print((endticks-startticks)/1000)
             elif(state == 'START'):
                 if(nextClientButton.checkClick(pygame.mouse.get_pos())):
+                    numberOfItems = random.randint(1, 5)
                     screen.blit(background, nextClientButton.rect)
                     state = 'GAME'
+            elif(state == 'GAMEOVER'):
+                for i in range(1, len(gameOverScreen)):
+                    if(gameOverScreen[i].checkClick(pygame.mouse.get_pos())):
+                        if(i == 1):
+                            screen.blit(background, gameOverScreen[0].rect)
+                            state = 'START'
+                        elif(i == 2):
+                            sys.exit(0)
 
-    if(state == 'GAME'):
-        if(len(itembutton.sprites()) == 0):
-            generateItem(itembutton)
-        itembutton.draw(screen)
-    elif(state == 'START'):
-        nextClientButton.draw(screen)
     buttons.draw(screen)
     textfield.draw(screen)
     otherUi.draw(screen)
+
+    if (state == 'GAME'):
+        if (len(itembutton.sprites()) == 0 and numberOfItemsDone < numberOfItems):
+            generateItem(itembutton)
+        elif (numberOfItemsDone >= numberOfItems):
+            state = 'START'
+            numberOfItems = 0
+            numberOfItemsDone = 0
+            numberOfGoods = 0
+        itembutton.draw(screen)
+    elif(state == 'START'):
+        nextClientButton.draw(screen)
+    elif(state == 'GAMEOVER'):
+        for g in gameOverScreen:
+            g.draw(screen)
+
     pygame.display.flip()

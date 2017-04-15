56caeaa0f9ce849eca37a11746cacd4d9f7bf9b5
Dawid Lubera <luberadawid@gmail.com>
2017-04-15 05:17:03 +0200

    Added Clickable class with click detection

--- main.py
+++ main.py
@@ -11,9 +11,20 @@ except ImportError as err:
     print('couldn\'t load module. %s' % err)
     sys.exit(1)
 
-class Button(pygame.sprite.Sprite):
+class Clickable(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.rect = Rect((0,0,0,0))
+
+    def checkClick(self, mousePos):
+        if(self.rect.collidepoint(mousePos[0], mousePos[1])):
+            return True
+        else:
+            return False
+
+class Button(Clickable):
+    def __init__(self):
+        Clickable.__init__(self)
         self.image, self.rect = load_img('button.png')
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
@@ -24,7 +35,7 @@ pygame.display.set_caption('xd')
 
 background = pygame.Surface(screen.get_size())
 background = background.convert()
-background.fill((127, 127, 127))
+background.fill((250, 250, 250))
 
 global button
 button = Button()
@@ -42,6 +53,9 @@ while True:
     for event in pygame.event.get():
         if(event.type == QUIT):
             sys.exit(2)
+        elif(event.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]):
+            for b in buttons.sprites():
+                print(b.checkClick(pygame.mouse.get_pos()))
     screen.blit(background, button.rect, button.rect)
     buttons.draw(screen)
     pygame.display.flip()

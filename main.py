5cd8b4cbc290f0f989e1e58f1f30bc8f96c1bd2c
Dawid Lubera <luberadawid@gmail.com>
2017-04-16 06:57:47 +0200

    More colors and small refactorization

--- main.py
+++ main.py
@@ -25,27 +25,26 @@ screensize = (1280, 720)
 def setValue(value, textfield):
     textfield.setText(str(int(textfield.string) * 10 + value))
 
+
 def backspace(textfield):
     textfield.setText(str(int(int(textfield.string) / 10)))
+
 
 def clear(textfield):
     textfield.setText('0')
 
+
 def unveilWeigh(itemButton):
     itemButton.sprites()[0].unveilWeigh()
+
 
 def initButtons(textfield, itembutton):
     buttonList = []
     buttonArgs = []
     start = [848, 360]
 
-    nextClientButton = TextFieldButton((50, screensize[1] - 150), (screensize[0] / 2 - 100, 100), 100, 'Next Client')
+    nextClientButton = TextFieldButton((50, screensize[1] - 150), (screensize[0] / 2 - 100, 100), 100, 'Next Client', (240, 240, 240))
 
-    gameOverAlert = TextFieldButton((200, screensize[1] / 2 - 200), (screensize[0] - 400, 400), 200, 'Game Over')
-    gameOverButton = TextFieldButton((screensize[0] / 2 - 300, screensize[1] / 2 + 100), (200, 64), 50, 'Try again')
-    gameOverExit = TextFieldButton((screensize[0] / 2 + 100, screensize[1] / 2 + 100), (200, 64), 50, 'Exit')
-
-    goodsAlert = TextFieldButton((200, screensize[1] / 2 - 50), (screensize[0] - 400, 100), 70, '')
     for i in range(3):
         for j in range(3):
             val = 1 + j + 3 * i
@@ -61,11 +60,22 @@ def initButtons(textfield, itembutton):
     buttonList.append(Button((start[0], start[1] + 2 * 80), 'Weigh', 40, 'longbutton.png', function=unveilWeigh))
     buttonArgs.append(itembutton)
 
-    return nextClientButton, (gameOverAlert, gameOverButton, gameOverExit), goodsAlert, RenderButton(buttonList), buttonArgs
+    return nextClientButton, RenderButton(buttonList), buttonArgs
+
+
+def initAlerts():
+    gameOverAlert = TextFieldButton((200, screensize[1] / 2 - 200), (screensize[0] - 400, 400), 200, 'Game Over', (240, 240, 240))
+    gameOverButton = TextFieldButton((screensize[0] / 2 - 300, screensize[1] / 2 + 100), (200, 64), 50, 'Try again')
+    gameOverExit = TextFieldButton((screensize[0] / 2 + 100, screensize[1] / 2 + 100), (200, 64), 50, 'Exit')
+
+    goodsAlert = TextFieldButton((200, screensize[1] / 2 - 50), (screensize[0] - 400, 100), 70, '', (240, 240, 240))
+
+    return (gameOverAlert, gameOverButton, gameOverExit), goodsAlert
+
 
 def generateItem(itembutton):
     x = random.randint(0, screensize[0] / 2 - 150)
-    y = random.randint(0, screensize[1] - 100)
+    y = random.randint(50, screensize[1] - 150)
     quantity = 1
     weigh = random.randint(0, 100) > 50
     itemName = weighItems[random.randint(0, len(weighItems) - 1)] if weigh else pieceItems[random.randint(0, len(pieceItems) - 1)]
@@ -79,6 +89,7 @@ def generateItem(itembutton):
 
     itembutton.add((ItemButton((x, y), itemName, quantity, weigh)))
 
+
 pygame.init()
 screen = pygame.display.set_mode(screensize)
 pygame.display.set_caption('The Great Rainbow')
@@ -87,10 +98,16 @@ background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((250, 250, 250))
 
-otherUi = pygame.sprite.RenderPlain((DrawRect((0, 0, 0), (screensize[0] / 2, 0), (2, screensize[1]))))
+otherUi = pygame.sprite.RenderPlain((
+    DrawRect((224, 224, 224), (0, 0), (screensize[0] / 2, screensize[1])),
+    DrawRect((64, 64, 64), (0, 50), (screensize[0] / 2, screensize[1] - 100)),
+    DrawRect((200, 224, 200), (screensize[0] / 2, 0), (screensize[0] / 2, screensize[1])),
+    DrawRect((0, 0, 0), (screensize[0] / 2, 0), (2, screensize[1])),
+))
 textfield = TextField((848, 120), (224, 64), 64, (190, 220, 165), (60, 85, 35))
 itembutton = RenderButton(())
-nextClientButton, gameOverScreen, goodsAlert, buttons, buttonArgs = initButtons(textfield, itembutton)
+nextClientButton, buttons, buttonArgs = initButtons(textfield, itembutton)
+gameOverScreen, goodsAlert = initAlerts()
 
 screen.blit(background, (0, 0))
 pygame.display.flip()
@@ -159,9 +176,9 @@ while True:
                         elif(i == 2):
                             sys.exit(0)
 
+    otherUi.draw(screen)
     buttons.draw(screen)
     textfield.draw(screen)
-    otherUi.draw(screen)
 
     if (state == 'START'):
         nextClientButton.draw(screen)
